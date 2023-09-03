44978cf3dacc7865d857e90b1746edada6a6b5d4
SlimyRedstone <49628477+SlimyRedstone@users.noreply.github.com>
2023-09-03 02:05:56 +0200

    Update background.js
    Fixed the extension will stop after it unloads

--- background.js
+++ background.js
@@ -6,6 +6,19 @@ var debugMode = false
 Object.prototype.getDefaultBool = function (key) {
 	return this.hasOwnProperty(key) ? this[key] == true : false
 }
+const encodeHTML = (value) => {
+	return /* encodeURI */ value
+		.trim()
+		.replace('"', '')
+		.replace(/[^A-Za-z0-9 _-]+/gi, '-')
+}
+const encodeHTMLSpaces = (value) => {
+	return /* encodeURI */ value
+		.trim()
+		.replace('"', '')
+		.replace(/[^A-Za-z0-9 _-]+/gi, '-')
+		.replace(/\s+/gi, '+')
+}
 
 chrome.runtime.onInstalled.addListener(function () {
 	let parentContext = chrome.contextMenus.create({
@@ -41,69 +54,63 @@ chrome.runtime.onInstalled.addListener(function () {
 				console.error('Malformed db.json !')
 			}
 		})
-})
 
-const encodeHTML = (value) => {
-	return /* encodeURI */ value
-		.trim()
-		.replace('"', '')
-		.replace(/[^A-Za-z0-9 _-]+/gi, '-')
-}
-const encodeHTMLSpaces = (value) => {
-	return /* encodeURI */ value
-		.trim()
-		.replace('"', '')
-		.replace(/[^A-Za-z0-9 _-]+/gi, '-')
-		.replace(/\s+/gi, '+')
-}
+	chrome.contextMenus.onClicked.addListener((data, tab) => {
+		console.log('Displaying "data" content')
+		console.table(data)
+		console.log('\n\nDisplaying "tab" content')
+		console.table(tab)
 
-chrome.contextMenus.onClicked.addListener((data, tab) => {
-	console.log('Displaying "data" content')
-	console.table(data)
-	console.log('\n\nDisplaying "tab" content')
-	console.table(tab)
+		// if (tab['status'] != 'completed') return
+		if (typeof data != 'object') return
+		if (!data.hasOwnProperty('menuItemId')) return
+		if (!data.hasOwnProperty('selectionText')) return
+		if (localDb.hasOwnProperty('error')) return
 
-	// if (tab['status'] != 'completed') return
-	if (typeof data != 'object') return
-	if (!data.hasOwnProperty('menuItemId')) return
-	if (!data.hasOwnProperty('selectionText')) return
-	if (localDb.hasOwnProperty('error')) return
+		console.log('1st check')
+		// const pageURL = new URL('https://www.google.com/search')
+		const selectedText = data.selectionText.toString()
 
-	// const pageURL = new URL('https://www.google.com/search')
-	const selectedText = data.selectionText.toString()
+		const selectedId = data.menuItemId.replace('#', '')
+		let breakForEach = false
+		console.log('2nd check')
+		console.log(localDb)
+		localDb['list'].forEach((key, index) => {
+			if (breakForEach) return
 
-	const selectedId = data.menuItemId.replace('#', '')
-	let breakForEach = false
-	localDb['list'].forEach((key, index) => {
-		if (breakForEach) return
-
-		if (key['id'] == selectedId) {
-			const pageURL = new URL(key['queryLink'], key['url'])
-			var queryStr = `${encodeHTML(selectedText)}`
-			if (key.hasOwnProperty('queryModifiers')) {
-				if (key['queryModifiers'].hasOwnProperty('prefix')) queryStr = `${key['queryModifiers']['prefix']} ${queryStr}`
-				if (key['queryModifiers'].hasOwnProperty('suffix')) queryStr = `${queryStr} ${key['queryModifiers']['suffix']}`
-			}
-			pageURL.searchParams.set(key['queryTag'], queryStr.trim())
-
-			if (key.hasOwnProperty('additionalQueries')) {
-				if (key['additionalQueries'].length > 0) {
-					key['additionalQueries'].forEach((q, qI) => {
-						pageURL.searchParams.set(q['name'], q['value'])
-					})
+			if (key['id'] == selectedId) {
+				console.log('3nd check')
+				const pageURL = new URL(key['queryLink'], key['url'])
+				console.log(pageURL.href)
+				var queryStr = `${encodeHTML(selectedText)}`
+				if (key.hasOwnProperty('queryModifiers')) {
+					if (key['queryModifiers'].hasOwnProperty('prefix')) queryStr = `${key['queryModifiers']['prefix']} ${queryStr}`
+					if (key['queryModifiers'].hasOwnProperty('suffix')) queryStr = `${queryStr} ${key['queryModifiers']['suffix']}`
 				}
+				pageURL.searchParams.set(key['queryTag'], queryStr.trim())
+				console.log('4th check')
+
+				if (key.hasOwnProperty('additionalQueries')) {
+					if (key['additionalQueries'].length > 0) {
+						key['additionalQueries'].forEach((q, qI) => {
+							pageURL.searchParams.set(q['name'], q['value'])
+						})
+					}
+				}
+				console.log('5th check')
+				const focusOnOpen = key.getDefaultBool('focusOnOpen')
+				const openNewTab = key.getDefaultBool('openNewTab')
+				const newTabIndex = openNewTab ? tab.index + 1 : tab.index
+				console.log('6th check')
+				if (debugMode) {
+					console.log(`Request to create new tab → ${pageURL.href}`)
+				} else {
+					chrome.tabs.create({ url: pageURL.href, index: newTabIndex, active: focusOnOpen })
+					console.log('Creating new tab with url: ', pageURL.href)
+				}
+				breakForEach = true
+				return
 			}
-			const focusOnOpen = key.getDefaultBool('focusOnOpen')
-			const openNewTab = key.getDefaultBool('openNewTab')
-			const newTabIndex = openNewTab ? tab.index + 1 : tab.index
-			if (debugMode) {
-				console.log(`Request to create new tab → ${pageURL.href}`)
-			} else {
-				chrome.tabs.create({ url: pageURL.href, index: newTabIndex, active: focusOnOpen })
-				console.log('Creating new tab with url: ', pageURL.href)
-			}
-			breakForEach = true
-			return
-		}
+		})
 	})
 })
